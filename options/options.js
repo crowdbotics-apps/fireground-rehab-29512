62cb1a78d6d9a80dfe7b95ef6ae2dbc4467b46aa
Crowdbotics <team@crowdbotics.com>
2021-08-05 15:20:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "fireground_rehab_29512Identifier",
+  url: "https://fireground_rehab_29512Identifier.botics.co",
+  api: "https://fireground_rehab_29512Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
